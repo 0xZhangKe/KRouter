3d7228b609f192a6fa22ac5bab185a7f23754299
ZhangKe <kezhang404@gmail.com>
2024-09-10 22:38:16 +0800

    prepare for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,32 +16,32 @@ allprojects {
 }
 
 
-allprojects {
-    group = "com.zhangke.krouter"
-    version = "0.9.0"
-
-    plugins.withId("om.vanniktech.maven.publish.base") {
-        mavenPublishing {
-            publishToMavenCentral(SonatypeHost.DEFAULT)
-            signAllPublications()
-
-            pom {
-                name.set("KRouter")
-                description.set("Lightweight Kotlin router.")
-                url.set("https://github.com/0xZhangKe/KRouter")
-                developers {
-                    developer {
-                        id.set("zhangke")
-                        name.set("Zhangke")
-                        url.set("https://github.com/0xZhangKe/KRouter")
-                    }
-                }
-                scm {
-                    url.set("https://github.com/0xZhangKe/KRouter")
-                    connection.set("scm:git:git://github.com/0xZhangKe/KRouter.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/0xZhangKe/KRouter.git")
-                }
-            }
-        }
-    }
-}
+//allprojects {
+//    group = "com.zhangke.krouter"
+//    version = "0.9.0"
+//
+//    plugins.withId("om.vanniktech.maven.publish.base") {
+//        mavenPublishing {
+//            publishToMavenCentral(SonatypeHost.DEFAULT)
+//            signAllPublications()
+//
+//            pom {
+//                name.set("KRouter")
+//                description.set("Lightweight Kotlin router.")
+//                url.set("https://github.com/0xZhangKe/KRouter")
+//                developers {
+//                    developer {
+//                        id.set("zhangke")
+//                        name.set("Zhangke")
+//                        url.set("https://github.com/0xZhangKe/KRouter")
+//                    }
+//                }
+//                scm {
+//                    url.set("https://github.com/0xZhangKe/KRouter")
+//                    connection.set("scm:git:git://github.com/0xZhangKe/KRouter.git")
+//                    developerConnection.set("scm:git:ssh://git@github.com/0xZhangKe/KRouter.git")
+//                }
+//            }
+//        }
+//    }
+//}
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
+    id("com.vanniktech.maven.publish")
 }
 
 kotlin {
@@ -19,5 +20,37 @@ kotlin {
             }
         }
     }
+
+    sourceSets{
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("reflect"))
+            }
+        }
+    }
 }
 
+mavenPublishing {
+    coordinates(libs.versions.krouter.group.get(), "core", libs.versions.krouter.version.get())
+
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.S01)
+
+    signAllPublications()
+    pom {
+        name.set("KRouter")
+        description.set("Lightweight Kotlin router.")
+        url.set("https://github.com/0xZhangKe/KRouter")
+        developers {
+            developer {
+                id.set("zhangke")
+                name.set("Zhangke")
+                url.set("https://github.com/0xZhangKe/KRouter")
+            }
+        }
+        scm {
+            url.set("https://github.com/0xZhangKe/KRouter")
+            connection.set("scm:git:git://github.com/0xZhangKe/KRouter.git")
+            developerConnection.set("scm:git:ssh://git@github.com/0xZhangKe/KRouter.git")
+        }
+    }
+}
