e32bd728cd19cc6b62c8baa2f31caf61c257ea94
ZhangKe <kezhang404@gmail.com>
2025-05-09 19:43:53 +0800

    upgrade to 1.2.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ object ProjectVersion {
     private const val MINOR = "2"
 
     // backwards compatible bug fixes
-    private const val PATH = "4"
+    private const val PATH = "5"
     const val VERSION = "$MAJOR.$MINOR.$PATH"
 }
 
